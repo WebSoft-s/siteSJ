8e1250eea039b637d509cb7de00bbc9a408dfe78
Ismael Borges <ismaelbb26@gmail.com>
2021-02-08 16:22:59 -0300

    animação ícone menu/ teste imagens receitas

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -4,10 +4,14 @@ const menuMobile = document.querySelector('.menuMobContainerMain');
 export default function handleClick() {
   if (menuMobile.className.indexOf('ativo') === -1) {
     menuMobile.classList.add('ativo');
+    btnMenu.classList.add('on');
     menuMobile.classList.remove('desativado');
+    btnMenu.classList.remove('off');
   } else if (menuMobile.className.indexOf('ativo') !== -1) {
-    menuMobile.classList.remove('ativo');
     menuMobile.classList.add('desativado');
+    btnMenu.classList.add('off');
+    menuMobile.classList.remove('ativo');
+    btnMenu.classList.remove('on');
   }
 }
 btnMenu.addEventListener('click', handleClick);
