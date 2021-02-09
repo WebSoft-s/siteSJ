190c2411637b773f7fda2b3f5784720880bf27f7
Ismael Borges <ismaelbb26@gmail.com>
2021-02-09 12:55:58 -0300

    commit obrigatorio

--- js/modalReceitas.js
+++ js/modalReceitas.js
@@ -1,5 +1,5 @@
 // receita 1
-function handleClick1() {
+/*function handleClick1() {
   document.querySelector('#receitaModal1').style.display = 'flex';
 }
 function fechar1() {
@@ -43,4 +43,23 @@ function handleClick6() {
 }
 function fechar6() {
   document.querySelector('#receitaModal2').style.display = 'none';
+}
+*/
+const modal1 = document.getElementById('receitaModal1');
+const modal2 = document.getElementById('receitaModal2');
+const modal3 = document.getElementById('receitaModal3');
+const modal4 = document.getElementById('receitaModal4');
+const modal5 = document.getElementById('receitaModal5');
+const modal6 = document.getElementById('receitaModal6');
+
+function ativarModal(modal) {
+  modal.style.display = 'flex';
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
+function fecharModal(modal) {
+  modal.style.display = 'none';
 }
