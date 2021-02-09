580ea8a3ca5bf6933e9ee798958e39e22d8f7000
Ismael Borges <ismaelbb26@gmail.com>
2021-02-09 14:07:05 -0300

    receitas prontas

--- js/modalReceitas.js
+++ js/modalReceitas.js
@@ -1,6 +1,19 @@
-const modal1 = document.getElementById('receitaModal1');
-const modal2 = document.getElementById('receitaModal2');
-const modal3 = document.getElementById('receitaModal3');
-const modal4 = document.getElementById('receitaModal4');
-const modal5 = document.getElementById('receitaModal5');
-const modal6 = document.getElementById('receitaModal6');
+const modal1 = document.getElementById("receitaModal1");
+const modal2 = document.getElementById("receitaModal2");
+const modal3 = document.getElementById("receitaModal3");
+const modal4 = document.getElementById("receitaModal4");
+const modal5 = document.getElementById("receitaModal5");
+const modal6 = document.getElementById("receitaModal6");
+
+function ativarModal(modal) {
+    modal.style.display = "flex"
+
+    window.onclick = function(event) {
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+    }
+}
+function fecharModal(modal){
+    modal.style.display = "none"
+}
