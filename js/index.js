fdd034626f70389bd33ac5007fb587a593d26940
Ismael Borges <ismaelbb26@gmail.com>
2021-02-10 16:10:16 -0300

    Ajuste lista menu mobile 10/02 16h

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import SlideNav from './slide.js';
 import menuMobile from './menuMobile.js';
-
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,5 +1,7 @@
 const btnMenu = document.querySelector('.menuMobileIcone');
 const menuMobile = document.querySelector('.menuMobContainerMain');
+const btnSobre = document.querySelector('#btnSobre');
+const lista = document.querySelectorAll('.MenuMobLista ul li a'); // seletor 'ALL' que vai selecionar todas as li's do menuMobile
 
 export default function handleClick() {
   if (menuMobile.className.indexOf('ativo') === -1) {
@@ -14,4 +16,17 @@ export default function handleClick() {
     btnMenu.classList.remove('on');
   }
 }
+//quando clicar fora da lista do menuMobile ativa a função novamente fechando o menu
+window.onclick = function (event) {
+  if (event.target === menuMobile) {
+    handleClick();
+  }
+};
+
+// adiciona um evento de click ao botão do icone do menuMobile e chama a função
 btnMenu.addEventListener('click', handleClick);
+
+/* O forEach é usado quando estamos selecionando mais de um item com o mesmo nome, nesse caso as li's, para cada li vai ter um evento de click separado que vai ativar a função novamente */
+lista.forEach((li) => {
+  li.addEventListener('click', handleClick);
+});
