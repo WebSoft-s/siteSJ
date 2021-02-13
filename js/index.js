8807b0a44d8c40bfff0ed9cec2f9fff905c8b696
JoãoDuarte10 <joaoduarte7341@gmail.com>
2021-02-13 20:52:50 -0300

    Slide pronto

--- js/index.js
+++ js/index.js
@@ -1,7 +1,2 @@
-import SlideNav from './slide.js';
 import menuMobile from './menuMobile.js';
 import cardWhats from './cardWhats.js';
-
-const slide = new SlideNav('.slide', '.slide-wrapper');
-slide.init();
-slide.addArrow('.prev', '.next');
